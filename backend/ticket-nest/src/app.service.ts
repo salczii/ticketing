2f0e22f8726c01870505110c9c5a40c00ee54bae
Kacper Salek <kacper.salek32@gmail.com>
2021-03-01 22:48:16 +0100

    test commit

--- backend/ticket-nest/src/app.service.ts
+++ backend/ticket-nest/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'i am hacker!!!!!!!';
+    return 'i am hacker!!!!!!';
   }
 }
