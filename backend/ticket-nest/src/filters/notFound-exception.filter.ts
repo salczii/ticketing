9eaef9212dc84503b2dc775f62766f8bc8520ded
Kacper Salek <kacper.salek32@gmail.com>
2021-03-15 19:30:04 +0100

    add not found exception filter and handle unknown paths

---
+++ backend/ticket-nest/src/filters/notFound-exception.filter.ts
@@ -0,0 +1,15 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  NotFoundException,
+} from '@nestjs/common';
+
+@Catch(NotFoundException)
+export class NotFoundExceptionFilter implements ExceptionFilter {
+  catch(exception: NotFoundException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    response.send('Page Not Found');
+  }
+}
--- backend/ticket-nest/src/main.ts
+++ backend/ticket-nest/src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NotFoundExceptionFilter } from './filters/notFound-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new NotFoundExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
